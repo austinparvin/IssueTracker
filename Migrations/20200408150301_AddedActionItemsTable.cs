a68bb1f9c587654f45e9acdd963f2dd665c6fce0
Austin Parvin <austin.parvin@gmail.com>
2020-04-08 11:03:56 -0400

    migrations

---
+++ Migrations/20200408150301_AddedActionItemsTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace IssueTracker.Migrations
+{
+    public partial class AddedActionItemsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActoinItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(nullable: true),
+                    IsChecked = table.Column<bool>(nullable: false),
+                    IssueId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActoinItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActoinItems_Issues_IssueId",
+                        column: x => x.IssueId,
+                        principalTable: "Issues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActoinItems_IssueId",
+                table: "ActoinItems",
+                column: "IssueId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActoinItems");
+        }
+    }
+}
