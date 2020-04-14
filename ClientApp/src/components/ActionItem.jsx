e63ecb0c75590028250182ab17737efd0d91ff4b
Austin Parvin <austin.parvin@gmail.com>
2020-04-14 09:49:44 -0400

    tracking checkboxes successfully

--- ClientApp/src/components/ActionItem.jsx
+++ ClientApp/src/components/ActionItem.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const ActionItem = ({ actionItem }) => {
+  const [isChecked, setIsChecked] = useState(actionItem.isChecked)
+
+  const trackIsChecked = async () => {
+    actionItem.isChecked = !actionItem.isChecked
+    setIsChecked(actionItem.isChecked)
+    const resp = await axios.put(`api/actionitem/${actionItem.id}`, actionItem)
+    console.log(resp.data)
+  }
+
+  let opts = {}
+  if (isChecked) {
+    opts['checked'] = 'checked'
+  } else {
+    opts['checked'] = ''
+  }
+
   return (
     <div className="action-item">
       <div className="checkbox">
-        <input className="checkbox" type="checkbox" name="" id=""></input>
+        <input
+          onClick={trackIsChecked}
+          className="checkbox"
+          type="checkbox"
+          name=""
+          {...opts}
+          id=""
+        ></input>
       </div>
       <div className="description">
         <p>{actionItem.description}</p>
