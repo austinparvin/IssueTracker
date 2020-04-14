1669f13bbd495a90e52173a671ad74b2e4bacaec
Austin Parvin <austin.parvin@gmail.com>
2020-04-14 09:25:28 -0400

    styled the Issue Details action items

--- ClientApp/src/components/ActionItem.jsx
+++ ClientApp/src/components/ActionItem.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const ActionItem = ({ actionItem }) => {
   return (
-      
     <div className="action-item">
-      <input className="checkbox" type="checkbox" name="" id=""></input>
-      <p>{actionItem.description}</p>
+      <div className="checkbox">
+        <input className="checkbox" type="checkbox" name="" id=""></input>
+      </div>
+      <div className="description">
+        <p>{actionItem.description}</p>
+      </div>
     </div>
   )
 }
