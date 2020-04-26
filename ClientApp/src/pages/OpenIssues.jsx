a6b79408341679fa455cd748d2d707654c063ad0
Austin Parvin <austin.parvin@gmail.com>
2020-04-26 19:44:24 -0400

    Open Issues now also has loading spinner

--- ClientApp/src/pages/OpenIssues.jsx
+++ ClientApp/src/pages/OpenIssues.jsx
@@ -2,26 +2,32 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import IssueCard from '../components/IssueCard'
 import { useAuth0 } from '../react-auth0-spa'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 const OpenIssues = () => {
   const { getTokenSilently } = useAuth0()
 
   const [openIssues, setOpenIssues] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const getOpenIssues = async () => {
+    setIsLoading(true)
     // Get Token
     const token = await getTokenSilently()
     axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
 
     const resp = await axios.get('api/issue/open')
     setOpenIssues(resp.data)
+    setIsLoading(false)
   }
 
   useEffect(() => {
     getOpenIssues()
   }, [])
 
-  if (openIssues.length < 1) {
+  if (isLoading) {
+    return <LoadingSpinner />
+  } else if (openIssues.length < 1) {
     return (
       <section className="empty-my-issues">
         <header>There are no Open Issues</header>
